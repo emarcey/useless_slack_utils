c496fd9e68c0c649b9bac71233e82c8cd8099e98
Evan Marcey <evanmarcey@gmail.com>
2018-04-30 15:51:15 -0400

    Update slackEventHandler.py
    Added someones_talking_about_you

--- src/slackEventHandler.py
+++ src/slackEventHandler.py
@@ -16,29 +16,36 @@ class SlackEventHandler:
                  random_reply_flg=False,
                  set_typing_flg=False,
                  mark_read_flg=False,
+                 someones_talking_about_you_flg=False,
                  run_level="DM Only",
                  users=None,
-                 responses=None
+                 responses=None,
+                 stay_channel=None
                  ):
         """
         :param slack_token: (str) API token to connect to Slack
         :param random_reply_flg: (Bool) True if you want the handler to perform the random_reply handling
         :param set_typing_flg: (Bool) True if you want the handler to perform the set_typing handling
         :param mark_read_flg: (Bool) True if you want the handler to perform the mark_read handling
-        :param run_level: (str) Works on 3 levels: DM Only (only direct messages), Private (dms and private channels), and all
+        :param someones_talking_about_you_flg: (Bool) True if you want the handler to perform someones_talking_about_you
+            handling
+        :param run_level: (str) Works on 3 levels: DM Only (only direct messages), Private (dms and private channels),
+            and all
         :param users: ([str]) List of users for whom events should be handled
         :param responses: ([str]) If using the random_reply, this is the list of custom responses
+        :param stay_channel: (str) channel to use if you're doing someones_talking_about_you
         """
         self.slack_token = slack_token
         self.random_reply_flg = random_reply_flg
         self.set_typing_flg = set_typing_flg
         self.mark_read_flg = mark_read_flg
+        self.someones_talking_about_you_flg = someones_talking_about_you_flg
         self.run_level = run_level
         self.users = users
         if responses:
             self.responses = responses
         else:
-            #default responses if none provided
+            # default responses if none provided
             self.responses = [
                 'Wow! That\'s so interesting!',
                 'What hilarious hijinx you\'ve been getting up to!',
@@ -49,6 +56,7 @@ class SlackEventHandler:
                 'Thanks for letting me know.',
                 'I\'ll be sure to follow up on that.'
             ]
+        self.stay_channel=stay_channel
 
     def begin(self, length=-1):
         """
@@ -60,32 +68,47 @@ class SlackEventHandler:
         sc = SlackClient(self.slack_token)
         try:
             if sc.rtm_connect():
+                # get list of all users
+                all_users = [
+                    {
+                        'name': user['name'],
+                        'id': user['id'],
+                        'first_name': user['profile']['first_name'],
+                        'last_name': user['profile']['last_name']
+                    }
+                    for user in sc.api_call("users.list")['members']
+                    if 'first_name' in user['profile'].keys() and 'last_name' in user['profile'].keys()]
+
                 start_time = time.time()
-                while sc.server.connected and (time.time() <= start_time+length or length ==-1):
+
+                # connect to server and start monitoring
+                while sc.server.connected and (time.time() <= start_time+length or length == -1):
                     event = sc.rtm_read()
 
                     try:
                         if event:
                             logger.debug(event)
 
+                            # check event type and determine if action should be taken
                             msg_type = self.get_msg_type(sc, event)
                             logger.debug(msg_type)
                             if (self.run_level == 'DM Only' and msg_type == 'IM') or \
                                 (self.run_level == 'Private' and msg_type != 'Public') or\
                                     self.run_level == 'All':
 
-                                #if message is in correct scope, perform designated tasks
+                                # if message is in correct scope, perform designated tasks
                                 if self.random_reply_flg:
-                                    self.random_reply(sc, event, msg_type)
+                                    self.random_reply(sc, event)
                                 if self.mark_read_flg:
                                     self.mark_read(sc, event, msg_type)
+                                if self.someones_talking_about_you_flg:
+                                    self.someones_talking_about_you(sc, event, msg_type,all_users)
                             else:
                                 logger.debug("Message not in scope.")
                         time.sleep(1)
 
                     except KeyError:
-                        logger.debug(event)
-                        logger.debug("Ignore this event")
+                        logger.debug("Ignore this event: " + str(event))
 
                 if time.time() > start_time + length:
                     logger.debug("Event handling completed.\nStopping Slack monitor.")
@@ -161,3 +184,53 @@ class SlackEventHandler:
             else:
                 raise
 
+    def someones_talking_about_you(self, sc, event, msg_type, all_users):
+        """
+        :param sc: SlackClient used to connect to server
+        :param event: event to be handled by the mark_read
+        :param msg_type: type of message
+        :param all_users: all users in slack environment
+        :return:
+        """
+
+        try:
+            users_to_notify = []
+
+            if event[0]['type'] == 'message' and msg_type == 'IM':
+                text = event[0]['text']
+                for user in all_users:
+                    if find_element_in_string(text.lower(),
+                                              user['first_name'].lower() + ' ' + user['last_name'].lower()) != -1 :
+                        users_to_notify.append(user)
+
+                if len(users_to_notify) > 0:
+                    im_list = sc.api_call("im.list")
+                    user_ids = [user['id'] for user in users_to_notify]
+
+                    v_len = len(users_to_notify)
+                    not_all_users_in_convo = False
+                    convo_members = sc.api_call("conversations.members", channel=event[0]['channel'])
+                    for user in user_ids:
+                        if user not in [cv for cv in convo_members['members']]:
+                            not_all_users_in_convo = True
+
+                    if not_all_users_in_convo:
+                        message = """Hey <@{u}> 
+                        
+                        <@{c}> were talking about you in a private message!
+                        
+                        Here's what they said:
+                        
+                        {t}
+                        """.format(u="> <@".join(user_ids),
+                                   c="> <@".join([cv for cv in convo_members['members']]),
+                                   t=text)
+
+                        sc.rtm_send_message(self.stay_channel, message)
+
+        except KeyError:
+            if 'type' not in event[0].keys():
+                logger.debug("Don't worry about this one.")
+                logger.debug(event)
+            else:
+                raise
