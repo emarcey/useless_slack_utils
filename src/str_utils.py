743802611d4f227c017cd5291bd48147ed295f43
Evan Marcey <evanmarcey@gmail.com>
2018-05-01 12:27:29 -0400

    Update str_utils.py
    added strip_punctuation

--- src/str_utils.py
+++ src/str_utils.py
@@ -1,14 +1,30 @@
-def find_element_in_string(string,e):
+import string
+
+def find_element_in_string(s, e):
     """
     Returns -1 instead of ValueError for index()
 
     :Args:
-        - string (str): string to search
+        - s (str): string to search
         - e (str): element to search for
     :Returns:
         - either index of first location of e in string, or -1 if not found
     """
     try:
-        return string.index(e)
+        return s.index(e)
     except ValueError:
         return -1
+
+
+def strip_punctuation(s):
+    """
+    Removes all punctuation marks from the ends of a string
+
+    :param s: (str) string to be stripped
+    :return: string with all punctuation marks stripped off the sides
+    """
+
+    for ch in string.punctuation:
+        s = s.strip(ch)
+
+    return s
